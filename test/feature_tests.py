c4ffb5a0c43a7052b04d221ea957c385e5a95bba
guthriec <guthriec@stanford.edu>
2015-05-08 18:06:25 -0700

    better unit testing framework

--- test/feature_tests.py
+++ test/feature_tests.py
@@ -3,25 +3,38 @@ __author__ = 'guthriec'
 import features.features as features
 from nltk.corpus import wordnet as wn
 
-def add_failure(return_val, message):
-    return_val['passed'] = False
-    return_val['messages'].append(message)
+class TestResult:
+    def __init__(self, test_name):
+        self.passed = True
+        self.err_messages = []
+        self.test_name = test_name
+
+    def add_failure(self, message = ""):
+        self.passed = False
+        self.err_messages.append(message)
+
+    def full_messages(self):
+        if self.passed:
+            return [self.test_name + " passed"]
+        else:
+            out = [self.test_name + " failed"]
+            out.extend(self.err_messages)
+            return out
+
 
 def test_hypernyms():
     table_sent1 = "I hate when the table is too short."
     table_sent2 = "What a cute table!"
     furniture_sent = "That furniture is ugly."
     hot_sent = "Of the hot soups, it is the best."
-    return_val = {'passed': True, 'messages': []}
+    result = TestResult("Hypernyms")
     if not features.hypernym_features(table_sent1, furniture_sent)['contains_hypernyms:']:
-        add_failure(return_val, "Basic hypernym not captured")
+        result.add_failure("Basic hypernym not captured")
     if not features.hypernym_features(table_sent1, table_sent2)['contains_hypernyms:']:
-        add_failure(return_val, "Reflexive hypernym (i.e. dog -> dog) not captured")
+        result.add_failure("Reflexive hypernym (i.e. dog -> dog) not captured")
     if features.hypernym_features(table_sent1, hot_sent)['contains_hypernyms:']:
-        add_failure(return_val, "Hypernym false positive")
-    if return_val['passed']:
-        return_val['messages'] = ["Hypernym tests successful"]
-    return return_val
+        result.add_failure("Hypernym false positive")
+    return result
 
 def test_synsets():
     return False
@@ -29,14 +42,12 @@ def test_synsets():
 def test_antonyms():
     hot_sent = "Of the hot soups, it is the best."
     cold_sent = "It is the worst cold soup"
-    return_val = {'passed': True, 'messages': []}
+    result = TestResult('Antonyms')
     if features.antonym_features(cold_sent, cold_sent)[wn.lemma('cold.a.01.cold')]:
-        add_failure(return_val, "Antonym falsely detected")
+        result.add_failure("Antonym falsely detected")
     if not features.antonym_features(hot_sent, cold_sent)[wn.lemma('cold.a.01.cold')]:
-        add_failure(return_val, "Antonym failed to be detected")
-    if return_val['passed']:
-        return_val['messages'] = ["Antonym tests successful"]
-    return return_val
+        result.add_failure("Antonym failed to be detected")
+    return result
 
 def run_feature_tests(print_results=True):
     results = []
@@ -45,12 +56,12 @@ def run_feature_tests(print_results=True):
     success = True
     for result in results:
         if print_results:
-            print result['messages']
-        success = success and result['passed']
+            print result.full_messages()
+        success = success and result.passed
     if print_results and success:
         print "All tests successful"
     if print_results and not success:
         print "Test failure, see above for details"
     return success
 
-run_feature_tests()
+print run_feature_tests()
