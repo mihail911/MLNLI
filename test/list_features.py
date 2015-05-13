006923a4493c3ba03c4f1c6acc20f7c614414bd7
Christopher Billovits <cjbillov@stanford.edu>
2015-05-12 23:14:21 -0700

    Improved harness, added coloring.

--- test/list_features.py
+++ test/list_features.py
@@ -8,12 +8,9 @@ import os
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath((__file__))))
 sys.path.append(root_dir)
 
-class color:
-   RED = '\033[91m'
-   CYAN = '\033[96m'
-   END = '\033[0m'
-
 import features.features as feat
+from util.color import color, prettyPrint
+
 
 if __name__ == '__main__':
     sys.stdout.write(color.RED + "Possible Features: \n" + '--' * 10 + '\n' + color.CYAN)
--- test/run.py
+++ test/run.py
@@ -1,6 +1,9 @@
 __author__ = 'mihaileric'
 
 import os,sys
+import re
+import collections
+import time
 
 """Add root directory path"""
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath((__file__))))
@@ -8,36 +11,45 @@ sys.path.append(root_dir)
 os.chdir(root_dir)
 
 import logging
-from models.models import build_log_regression_model, evaluate_model, parameter_tune_log_reg
+from models.models import build_log_regression_model, evaluate_model, parameter_tune_log_reg, save_vectors
 from argparse import ArgumentParser
+from util.colors import color, prettyPrint
 
 parser = ArgumentParser('description = provide arguments for running model pipeline')
 parser.add_argument('--conf_file', help = 'name of configuration file ')
 arguments = parser.parse_args()
 
-params = {}
 
-#Loads the parameters set in the conf file and saves in a global dict.
+params = collections.defaultdict(list)
+# Loads the parameters set in the conf file and saves in a global dict.
+# TODO: regex split a little hacky
 with open(arguments.conf_file, 'r') as f:
-    model_line = f.readline().split(':')
-    params[model_line[0].strip()] = model_line[1].strip()
+	stream = f.readlines()
+	for line in stream:	
+		kv = re.split(r'[ ,:;]*', line.rstrip())
+		val = kv[1:] if (len(kv) > 2 or kv[0] == 'features') else kv[1]
+		params[ kv[0] ] = val
 
-    features_line = f.readline().split(':')
-    values = features_line[1].strip()
-    params[features_line[0].strip()] = [feat.strip() for feat in values.split(',')]
+prettyPrint("===" * 16 + "\nTraining model '{0}' ... ".format(params['model']), color.YELLOW)
+prettyPrint("With features: {0}".format(params['features']), color.YELLOW)
 
-    feature_name_line = f.readline().split(':')
-    params[feature_name_line[0].strip()] = feature_name_line[1].strip()
+start_train = time.time()
+# Build model by specifying vectorizer, feature selector, features
 
-
-print "Training model '{0}' ... ".format(params['model'])
-print "With features: {0}".format(params['features'])
-
-#Build model by specifying vectorizer, feature selector, features
 model, feat_vec, labels = build_log_regression_model(features = params['features'], file_name = params['feature_file'])
-#report = evaluate_model(pipeline = model, features = params['features'], feature_file_name = params['features']) #Returns a classification report
-best_model = parameter_tune_log_reg(model, feat_vec, labels)
-print evaluate_model(best_model, features = params['features'], file_name = params['feature_file'])
+if params['load_vectors'] != 'true':
 
-print "Finished training and evaluating model"
+	save_vectors(feat_vec, labels, params['feature_file'] + ".train")
+
+best_model = parameter_tune_log_reg(model, feat_vec, labels)
+
+end_train = time.time() 
+prettyPrint ("Finished training.  Took {0:.2f} seconds".format(end_train - start_train), color.RED)
+
+# Training set , Dev set evaluation
+evaluate_model(best_model, reader = 'sick_train_reader', features = params['features'], load_vec = params['load_vectors'])
+prettyPrint ('===' * 16, color.YELLOW)
+evaluate_model(best_model, features = params['features'], file_name = params['feature_file'] + ".dev", load_vec = params['load_vectors'])
+
+prettyPrint("Finished training and evaluating model\n" + "===" * 16, color.YELLOW)
 
